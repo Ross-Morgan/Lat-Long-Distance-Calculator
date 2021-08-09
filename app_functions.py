bec0148c12bd9b1c6691d1f57e7526fd52df96f1
Ross-Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2021-08-09 14:22:00 +0100

    Removed redundant variables

--- app_functions.py
+++ app_functions.py
@@ -1,7 +1,5 @@
 from math import radians, cos, sin, asin, sqrt
 
-EARTH_RADIUS = 6371
-
 _radius = {
     "KM" : 6372.8, 
     "MI" : 3959.9,
@@ -20,8 +18,6 @@ def haversine(lat1, lon1, lat2, lon2, is_rads, unit):
     else:
         delta_lat = lat2 - lat1
         delta_lon = lon2 - lon1
-        lat1 = lat1
-        lat2 = lat2
 
     a = sin(delta_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(delta_lon/2) ** 2
     c = 2 * asin(sqrt(a))
